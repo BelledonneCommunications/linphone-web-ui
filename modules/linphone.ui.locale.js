6632ab286fc0e94c7cfb2aab7bbc00c7ed9fe5ba
Yann Diorcet <yann.diorcet@belledonne-communications.com>
2013-06-24 16:46:03 +0200

    Web2: minor fix for locales

--- modules/linphone.ui.locale.js
+++ modules/linphone.ui.locale.js
@@ -1,33 +1,46 @@
 /*globals jQuery,linphone*/
 
 linphone.ui.locale = {
-	currentLocale: function(base) {
+	currentLocale: function(base, log) {
 		var locales = linphone.ui.configuration(base).locales;
-		var locale = (navigator.language) ? navigator.language : navigator.userLanguage;
-		locale = locale.replace('-', '_');
-		linphone.core.log('Browser language: ' + locale);
+		if(typeof log === 'undefined') log = true;
+		
+		// Get saved locale or browser one
 		if (typeof linphone.ui.data(base).locale !== "undefined") {
-			return linphone.ui.data(base);
+			if(log) {
+				linphone.core.log('Saved locale: ' + linphone.ui.data(base).locale);
+			}
+			locale = linphone.ui.data(base).locale;
 		} else {
-			// Excat Match
-			for (var a in locales) {
-				if (locales[a].locale === locale) {
-					linphone.core.log('Exact locale: ' + locales[a].locale);
-					return locales[a];
-				}
+			var locale = (navigator.language) ? navigator.language : navigator.userLanguage;
+			locale = locale.replace('-', '_');
+			if(log) {
+				linphone.core.log('Browser language: ' + locale);
 			}
-
-			// Start Match
-			for (var b in locales) {
-				if (locales[b].locale.search(locale) === 0) {
-					linphone.core.log('Approx locale: ' + locales[b].locale);
-					return locales[b];
-				}
-			}
-
-			// Take default (first)
-			return locales[0];
 		}
+		
+		// Excat Match
+		for (var a in locales) {
+			if (locales[a].locale === locale) {
+				if(log) {
+					linphone.core.log('Exact locale: ' + locales[a].locale);
+				}
+				return locales[a];
+			}
+		}
+
+		// Start Match
+		for (var b in locales) {
+			if (locales[b].locale.search(locale) === 0) {
+				if(log) {
+					linphone.core.log('Approx locale: ' + locales[b].locale);
+				}
+				return locales[b];
+			}
+		}
+
+		// Take default (first)
+		return locales[0];
 	},
 	update: function(base) {
 		var locale = linphone.ui.locale.currentLocale(base);
@@ -43,7 +56,7 @@ linphone.ui.locale = {
 		linphone.ui.locale.uiInit(base);
 	},
 	uiInit: function(base) {
-		var locale = linphone.ui.locale.currentLocale(base);
+		var locale = linphone.ui.locale.currentLocale(base, false);
 		jQuery.i18n.data = linphone.ui.i18n;
 		jQuery.i18n.locale = locale;
 	},
