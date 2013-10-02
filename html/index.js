46003cc54650cb10e1064291e0b6fd5d4b04d091
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2013-10-02 16:02:07 +0200

    Update plugin version to 0.0.2.6.

--- html/index.js
+++ html/index.js
@@ -10,7 +10,7 @@
  */
 
 function __linphone_init(base) {
-	var pluginVersion = '0.0.2.5';
+	var pluginVersion = '0.0.2.6';
 	var activeXPluginVersion = pluginVersion.replace(/\./g, ',');
 
 	/* @if env='release' */
