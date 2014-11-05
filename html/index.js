6e78b14f63b8406db5493c9ccf45753a7d54a631
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2014-11-05 15:03:42 +0100

    Update plugin version to 1.0.9.

--- html/index.js
+++ html/index.js
@@ -10,7 +10,7 @@
  */
 
 function __linphone_init(base) {
-	var pluginVersion = '1.0.8';
+	var pluginVersion = '1.0.9';
 	var activeXPluginVersion = pluginVersion.replace(/\./g, ',');
 
 	/* @if env='release' */
